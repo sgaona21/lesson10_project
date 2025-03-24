e6b0e6bf1504167df379e218f965da263a0f772e
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-24 16:40:23 -0700

    variables created

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 Steven Gaona STE2342585
 Date: 3/24/25
 */
+
+const myForm = document.getElementById("myForm");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const submitButton = document.querySelector("button")
+const formError = document.getElementById("formError");
+const successMessage = document.getElementById("successMessage");
+
+
+
