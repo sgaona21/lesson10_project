aadfc1fd3c3d1559648e2a697fe7131bd55e3be3
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-24 18:54:50 -0700

    logic complete

--- js/main.js
+++ js/main.js
@@ -12,5 +12,57 @@ const submitButton = document.querySelector("button")
 const formError = document.getElementById("formError");
 const successMessage = document.getElementById("successMessage");
 
+let nameValid = true; 
+let emailValid = true;
 
 
+ function clearErrorMessage(message) {
+    message.innerText = ""
+ }
+
+function nameValidation() {
+    let nameRegEx = /^[a-zA-Z\s]+$/;
+
+    if (nameInput.value.trim() == "") {
+        nameError.innerText = "Name field cannot be blank";
+        nameValid = false;
+    } else if (!nameRegEx.test(nameInput.value.trim())) {
+        nameError.innerText = "Name field must contain only letters and spaces";
+        nameValid = false;
+    } else {
+        nameValid = true
+        clearErrorMessage(nameError)
+    }
+}
+
+function emailValidation() {
+    let emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (emailInput.value.trim() == "") {
+        emailError.innerText = "Email field cannot be empty";
+        emailValid = false;
+    } else if (!emailRegEx.test(emailInput.value.trim())) {
+        emailError.innerText = "Please use a valid email format";
+        emailValid = false;
+    } else {
+        emailValid = true
+        clearErrorMessage(emailError)
+    }
+}
+
+function formValidation() {
+    if (!nameValid || !emailValid) {
+        formError.innerText = "Please make sure form is filled out correctly"
+        clearErrorMessage(successMessage)
+    } else if (nameValid && emailValid) {
+        successMessage.innerText = "Form submitted successfully!"
+        clearErrorMessage(formError)
+    } 
+}
+
+myForm.addEventListener("submit", () => {
+    event.preventDefault();
+    nameValidation();
+    emailValidation();
+    formValidation();
+});
